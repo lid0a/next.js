5075f069b1979498bbfbd2032c8ab1711f4c108a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-06 15:57:42 +0900

    Fix more

--- turbopack/crates/turbopack-ecmascript/src/analyzer/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/analyzer/mod.rs
@@ -3837,7 +3837,7 @@ fn is_unresolved_id(i: &Id, unresolved_mark: Mark) -> bool {
 #[doc(hidden)]
 pub mod test_utils {
     use anyhow::Result;
-    use turbo_tasks::{FxIndexMap, Vc};
+    use turbo_tasks::{FxIndexMap, ResolvedVc, Vc};
     use turbopack_core::{compile_time_info::CompileTimeInfo, error::PrettyPrintError};
 
     use super::{
@@ -3893,7 +3893,7 @@ pub mod test_utils {
                     map.insert("./c".into(), format!("[context: {}]/c", options.dir).into());
 
                     JsValue::WellKnownFunction(WellKnownFunctionKind::RequireContextRequire(
-                        Vc::cell(map),
+                        ResolvedVc::cell(map),
                     ))
                 }
                 Err(err) => v.into_unknown(true, PrettyPrintError(&err).to_string()),
